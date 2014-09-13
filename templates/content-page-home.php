8699a785d835428de6931f012f7d2a945a88e475
Ian Noyes <ian@12southmusic.com>
2014-09-13 14:14:41 -0500

    home page

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,4 +1,5 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php the_content(); ?>
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
-<?php endwhile; ?>
+<div class='row'>
+	<div class='col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1'>
+		<?php echo do_shortcode('[wpv-view name="HOME | NEWS"]'); ?>
+	</div>
+</div>
