30d91981949f2eb83910ca73ad1ef4bc277f34f9
Ian Noyes <ian@12southmusic.com>
2014-09-14 14:20:48 -0500

    video page template

---
+++ template-video.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Template Name: Video Template
+*/
+?>
+
+<?php get_template_part('templates/content', 'page-video'); ?>
---
+++ templates/content-page-video.php
@@ -0,0 +1,5 @@
+<div class='row'>
+	<div class='col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2'>
+		<?php echo do_shortcode('[wpv-view name="Video Page"]'); ?>
+	</div>
+</div>
