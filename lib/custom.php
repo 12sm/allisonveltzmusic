d85af11a9fface39d1c8380d977db7f5fc06d5c6
Ian Noyes <ian@12southmusic.com>
2014-09-15 14:26:56 -0500

    echo $url

--- lib/custom.php
+++ lib/custom.php
@@ -5,10 +5,10 @@ function get_Embed($atts){
 	$defaults_array = array('url' => null);
 	  shortcode_atts( $defaults_array, $atts );
 	  $url = $atts['url'];
-	  preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $link, $matches);
-
+	  preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $url, $matches);
 	$id = $matches[0];
 	echo $id;
+	echo $url;
 	return  "http://www.youtube.com/embed/" . $id;
 }
 
