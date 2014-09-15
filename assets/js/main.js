372b1ca8f67d9d79e059595f23a7b79bcd166d52
Ian Noyes <ian@12southmusic.com>
2014-09-15 16:43:16 -0500

    audio js madness

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,46 @@ var Roots = {
   // All pages
   common: {
     init: function() {
+
+      audiojs.events.ready(function() {
+      var a = audiojs.createAll({
+        trackEnded: function() {
+            var next = $('ol li.playing').next();
+            if (!next.length) next = $('ol li').first();
+            next.addClass('playing').siblings().removeClass('playing');
+            audio.load($('a', next).attr('data-src'));
+            audio.play();
+      }
+      });
+      // Load in the first track
+        var audio = a[0];
+            first = $('ol li a').attr('data-src');
+        $('ol li').first().addClass('playing');
+        audio.load(first);
+
+        // Load in a track on click
+        $('ol li').click(function(e) {
+          e.preventDefault();
+          $(this).addClass('playing').siblings().removeClass('playing');
+          audio.load($('a', this).attr('data-src'));
+          audio.play();
+        });
+        // Keyboard shortcuts
+             // right arrow
+          $('.next-track').click( function(){
+            var next = $('li.playing').next();
+            if (!next.length) next = $('ol li').first();
+            next.click();
+            });
+            // back arrow
+           $('.prev-track').click(function(){
+            var prev = $('li.playing').prev();
+            if (!prev.length) prev = $('ol li').last();
+            prev.click();
+          });
+      });
+
+
       $('.fancyvid').fancybox({
             'width'     : 720,
             'height'        : 480,
