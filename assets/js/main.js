8a00a40a2ab57d42911443013c07d9690d01f9e1
Ian Noyes <ian@12southmusic.com>
2014-09-15 16:06:47 -0500

    lots of sound player stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,23 @@ var Roots = {
 
  single_music: {
     init: function(){
-      alert('single-music firing!');
+
+soundManager.setup({
+  // disable or enable debug output
+  debugMode: true,
+  // use HTML5 audio for MP3/MP4, if available
+  preferFlash: false,
+  useFlashBlock: true,
+  // path to directory containing SM2 SWF
+  url: '/assets/js',
+  // optional: enable MPEG-4/AAC support (requires flash 9)
+  flashVersion: 9
+  });
+
+      soundManager.onready(function() {
+    // soundManager.createSound() etc. may now be called
+    inlinePlayer = new InlinePlayer();
+  });
       /** Lyrics display **/
         $('.play').on('click', function(e){
             var lyrics = $(this).find('.lyric-output').clone();
