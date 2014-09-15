483f81930560985357a02454fa6b93281e71a9dd
Ian Noyes <ian@12southmusic.com>
2014-09-15 14:12:10 -0500

    cusotm get embed

--- lib/custom.php
+++ lib/custom.php
@@ -1,4 +1,18 @@
-<?php 
+<?php
+
+add_shortcode('video-embed', 'get_Embed');
+function get_Embed($atts){
+	$defaults_array = array('url' => null);
+	  shortcode_atts( $defaults_array, $atts );
+	  $url = $atts['url'];
+	preg_match(
+	        '/[\\?\\&]v=([^\\?\\&]+)/',
+	        $url,
+	        $matches
+	    );
+	$id = $matches[1];
+	echo  value="http://www.youtube.com/embed/" . $id;
+}
 
 define( 'UPLOADS', ''.'assets' );
 
