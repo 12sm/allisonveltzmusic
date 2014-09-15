b0380c4bbb2a7c0be8b04dbab3bdebcf02ad57ef
Ian Noyes <ian@12southmusic.com>
2014-09-15 14:25:36 -0500

    new preg_matches

--- lib/custom.php
+++ lib/custom.php
@@ -5,12 +5,10 @@ function get_Embed($atts){
 	$defaults_array = array('url' => null);
 	  shortcode_atts( $defaults_array, $atts );
 	  $url = $atts['url'];
-	preg_match(
-	        '/[\\?\\&]v=([^\\?\\&]+)/',
-	        $url,
-	        $matches
-	    );
+	  preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $link, $matches);
+
 	$id = $matches[0];
+	echo $id;
 	return  "http://www.youtube.com/embed/" . $id;
 }
 
