e34c2ea47564f9e70ae3d696f6305062e33e4196
Ian Noyes <ian@12southmusic.com>
2014-09-15 16:56:23 -0500

    divs and stuff

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,6 +1,5 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
-  <div class="container">
-    <div class="navbar-header">
+  <div class="container-fluid">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
@@ -10,7 +9,7 @@
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     </div>
 
-    <nav class="collapse navbar-collapse" role="navigation">
+    <nav class="collapse navbar-collapse col" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
