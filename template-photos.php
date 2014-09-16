bd54518e30c2d7bf0e93096ab681ceb0540240a2
Ian Noyes <ian@12southmusic.com>
2014-09-15 19:49:55 -0500

    flexslider

---
+++ template-photos.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Template Name: Photos Template
+*/
+?>
+
+<?php get_template_part('templates/content', 'page-photos'); ?>
---
+++ templates/content-page-photos.php
@@ -0,0 +1,14 @@
+<div class='row'>
+	<div class="col-sm-12 photo-full-width">
+		<div id='slider' class="flexslider">
+			[photos gallery="promo"]
+		</div>
+	</div>
+</div>
+<div class='row'>
+	<div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+		<div id='carousel' class="flexslider">
+			[photos gallery="promo"]
+		</div>
+	</div>
+</div>
