a77cbd99f5c0f2a247166ae61d8c65afca5647c9
Ian Noyes <ian@12southmusic.com>
2014-09-15 21:32:30 -0500

    new stuff

--- templates/content-page-photos.php
+++ templates/content-page-photos.php
@@ -1,14 +1,2 @@
-<div class='row'>
-	<div class="col-sm-12 photo-full-width">
-		<div id='slider' class="flexslider">
-			<?php echo do_shortcode('[photos gallery="promo"]'); ?>
-		</div>
-	</div>
-</div>
-<div class='row'>
-	<div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
-		<div id='carousel' class="flexslider">
-			<?php echo do_shortcode('[photos gallery="promo"]'); ?>
-		</div>
-	</div>
-</div>
+<?php echo do_shortcode('[wpv-view name="PHOTOS | GALLERY | FEATURED"]'); ?>
+
