2fbda1507a283f81928c80a9b59fef204ee9257f
Ian Noyes <ian@12southmusic.com>
2014-09-16 12:08:53 -0500

    logo in header

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="/assets/img/logo.png" class="img-responsive"></a>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
