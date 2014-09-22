4f4bb0f245c878116bd8ae2af30c4a208d99ad98
Sarah <Sarah@12southmusic.com>
2014-09-22 14:27:48 -0500

    ldkfjklsdf

--- templates/content-page-video.php
+++ templates/content-page-video.php
@@ -1,12 +1,6 @@
 <div class='row'>
 	<div class='col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2'>
-		<div class="row">
-			<div class="col-sm-2"></div>
-			<div class="col-sm-8">
-				<img src="http://placehold.it/350x150" />
-			</div>
-			<div class="col-sm-2"></div>
-		</div>
+		
 
 		<?php echo do_shortcode('[wpv-view name="Video Page"]'); ?>
 	</div>
