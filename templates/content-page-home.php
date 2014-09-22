44a7655573dc9dcd19674ccc9445f8dec24f9c73
Sarah <Sarah@12southmusic.com>
2014-09-22 15:50:54 -0500

    let see if this works

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,5 +1,6 @@
 <div class='row'>
-	<div class='col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2'>
+	<div class='col-sm-6 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2'>
+		<img src="/wp-content/themes/allisonveltzmusic/assets/img/logo-big.jpg" />
 		<?php echo do_shortcode('[wpv-view name="HOME | NEWS"]'); ?>
 	</div>
 </div>
