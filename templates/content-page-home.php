e78fe39a80046e45c15f05c56b5a7658cbe23edb
Sarah <Sarah@12southmusic.com>
2014-09-22 16:03:37 -0500

    cols

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,5 +1,5 @@
 <div class='row'>
-	<div class='col-sm-5 col-md-5 col-lg-8 col-lg-offset-2'>
+	<div class='col-xs-12 col-sm-5 col-md-5 col-lg-5'>
 		<img src="/wp-content/themes/allisonveltzmusic/assets/img/logo-big.png" class="img-responsive" />
 		<?php echo do_shortcode('[wpv-view name="HOME | NEWS"]'); ?>
 	</div>
