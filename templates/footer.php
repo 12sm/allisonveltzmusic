50d0bb16986d09067b9e80df393d989299665c54
Sarah <Sarah@12southmusic.com>
2014-09-24 11:43:32 -0500

    footer col's

--- templates/footer.php
+++ templates/footer.php
@@ -1,22 +1,24 @@
 <footer class="content-info container" role="contentinfo">
   <div class="row">
-    <div class="col-lg-12">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
       <div class="row sidebar-footer">
       	<?php dynamic_sidebar('sidebar-footer'); ?>
-    </div>
-    <div class="row footer-nav">  
-        <?php
-     if (has_nav_menu('footer_navigation')) :
-       wp_nav_menu(array('theme_location' => 'footer_navigation', 'menu_class' => ''));
-     endif;
-  ?>
-    </div>
-         
-		<div id="social" class="row social-nav">
-			<div class="col-sm-6">
-			Email Sign Up
+      </div>
+	  <div class="row footer-nav"> 
+			<div class="col-sm-12"> 
+				<?php
+			 if (has_nav_menu('footer_navigation')) :
+			   wp_nav_menu(array('theme_location' => 'footer_navigation', 'menu_class' => ''));
+			 endif;
+		  ?>
 			</div>
-			<div class="col-sm-6">
+	  </div>
+         
+	<div id="social" class="row social-nav">
+			<div class="col-sm-6 col-md-6 col-lg-6">
+			  Email Sign Up
+			</div>
+			<div class="col-sm-6 col-md-6 col-lg-6">
 				  <?php
 					 if (has_nav_menu('social_navigation')) :
 					   wp_nav_menu(array('theme_location' => 'social_navigation', 'menu_class' => ''));
@@ -24,7 +26,7 @@
 				  ?>
 				  <p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></p>
 			</div>
-		</div>
+	</div>
       
     </div>
   </div>
