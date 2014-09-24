3feb45bcb8d8f7cfe2d8a9575518781e786933a3
Sarah <Sarah@12southmusic.com>
2014-09-24 12:57:01 -0500

    width?

--- templates/footer.php
+++ templates/footer.php
@@ -6,7 +6,7 @@
       </div>
 	  <div class="row footer-nav"> 
 			<div class="col-xs-12 col-sm-4 col-md-4 col-lg-5 email-signup">
-			  <a href="https://app.e2ma.net/app2/audience/signup/1763819/1738434/?v=a" onclick="tab.open('https://app.e2ma.net/app2/audience/signup/1763819/1738434/?v=a', 'signup', 'menubar=no, location=no, toolbar=no, scrollbars=yes, height=500'); return false;">Join Allison's Exclusive Email List</a>
+			  <a href="https://app.e2ma.net/app2/audience/signup/1763819/1738434/?v=a" onclick="window.open('https://app.e2ma.net/app2/audience/signup/1763819/1738434/?v=a', 'signup', 'menubar=no, location=no, toolbar=no, scrollbars=yes, height=500, width=400'); return false;">Join Allison's Exclusive Email List</a>
 			</div>
 			<div class="col-xs-12 col-sm-5 col-md-5 col-lg-4"> 
 				<?php
