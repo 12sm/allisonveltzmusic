aeba31ee34e0fc250083b58d5a117dc025b3efec
Ian Noyes <ian@12southmusic.com>
2014-10-01 14:45:22 -0500

    email s9ignup experiments

--- templates/footer.php
+++ templates/footer.php
@@ -6,7 +6,7 @@
       </div>
 	  <div class="row footer-nav"> 
 			<div class="col-xs-12 col-sm-4 col-md-4 col-lg-5 email-signup">
-			  <a href="https://app.e2ma.net/app2/audience/signup/1763819/1738434/?v=a" onclick="window.open('https://app.e2ma.net/app2/audience/signup/1763819/1738434/?v=a', 'signup', 'menubar=no, location=no, toolbar=no, scrollbars=yes, height=580, width=650'); return false;">Join Allison's Exclusive Email List</a>
+			  <link href="https://app.e2ma.net/css/signup.sml.css" rel="stylesheet" type="text/css"><script type="text/javascript" src="https://app.e2ma.net/app2/audience/tts_signup/1763819/55ccffe82334411b90c492e32a9982ae/1738434/?v=a"></script><div id="load_check" class="signup_form_message" >This form needs Javascript to display, which your browser doesn't support. <a href="https://app.e2ma.net/app2/audience/signup/1763819/1738434/?v=a"> Sign up here</a> instead </div><script type="text/javascript">signupFormObj.drawForm();</script>
 			</div>
 			<div class="col-xs-12 col-sm-5 col-md-5 col-lg-4"> 
 				<?php
