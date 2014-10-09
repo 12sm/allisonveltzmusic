40e424965b90bc5a8c4d13e19f04297a208de526
Ian Noyes <ian@12southmusic.com>
2014-10-09 13:25:12 -0500

    this too

--- assets/js/sm2.js
+++ assets/js/sm2.js
@@ -1,22 +1,11 @@
-soundManager.setup({
-          // disable or enable debug output
-          debugMode: true,
-          // use HTML5 audio for MP3/MP4, if available
-          preferFlash: false,
-          useFlashBlock: true,
-          // path to directory containing SM2 SWF
-          url: '/assets/js',
-          // optional: enable MPEG-4/AAC support (requires flash 9)
-          flashVersion: 9
-          });
 
               soundManager.onready(function() {
             // soundManager.createSound() etc. may now be called
             inlinePlayer = new InlinePlayer();
             //pause functionality
             $('.audiojs').click(function(){
-              debugger;
             window.inlinePlayer.stopSound(inlinePlayer.lastSound);
+            $('.sm2_playing').removeClass('sm2_playing');
             });
           });
-      soundManager.stop();
+      
