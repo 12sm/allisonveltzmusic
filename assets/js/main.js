72080d62b0234c19c3c81e4feadf49cd365687a4
Ian Noyes <ian@12southmusic.com>
2014-10-09 12:46:54 -0500

    not in hurr

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,28 +15,6 @@
  * ======================================================================== */
 
 (function($) {
-      soundManager.setup({
-          // disable or enable debug output
-          debugMode: true,
-          // use HTML5 audio for MP3/MP4, if available
-          preferFlash: false,
-          useFlashBlock: true,
-          // path to directory containing SM2 SWF
-          url: '/assets/js',
-          // optional: enable MPEG-4/AAC support (requires flash 9)
-          flashVersion: 9
-          });
-
-              soundManager.onready(function() {
-            // soundManager.createSound() etc. may now be called
-            inlinePlayer = new InlinePlayer();
-            //pause functionality
-            $('.play-pause>.play').click(function(){
-              debugger;
-            window.inlinePlayer.stopSound(inlinePlayer.lastSound);
-            });
-          });
-      soundManager.stop();
 
 // Use this variable to set up the common and page specific functions. If you
 // rename this variable, you will also need to rename the namespace below.
