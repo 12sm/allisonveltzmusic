31f993aece3df997918b0d1e4e19987fc68c2dbf
michael <michael@12southmusic.com>
2014-10-20 14:09:17 -0500

    Turn comments back on

--- templates/content-single.php
+++ templates/content-single.php
@@ -3,14 +3,12 @@
     <div class="entry-content">
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
       <?php the_content(); ?>
+      	<div class="row">
+		<div class="black-bg post-comments-wrapper col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
+			<?php comments_template('/templates/comments.php'); ?>
+		</div>
+	</div>
     </div>
-    <footer>
-    </footer>
-	<div class="row">
-	<div class="black-bg post-comments-wrapper col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
-    <?php comments_template('/templates/comments.php'); ?>
-	</div>
-	</div>
   </article>
   
 <?php endwhile; ?>
