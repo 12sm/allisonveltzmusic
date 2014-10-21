4de6a0979b79d31417bc8ce50f274d055425d73b
michael <michael@12southmusic.com>
2014-10-21 15:50:03 -0500

    fix audio

--- assets/js/sm2.js
+++ assets/js/sm2.js
@@ -2,19 +2,6 @@
 
 $(document).ready(function() {
 
- soundManager.setup({
-      	// disable or enable debug output
-        debugMode: true,
-        // use HTML5 audio for MP3/MP4, if available
-        preferFlash: false,
-        useFlashBlock: true,
-        // path to directory containing SM2 SWF
-        url: '/assets/js',
-        // optional: enable MPEG-4/AAC support (requires flash 9)
-        flashVersion: 9
-      });
-
-
 soundManager.onready(function() {
 	// soundManager.createSound() etc. may now be called
     inlinePlayer = new InlinePlayer();
