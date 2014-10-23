44d185126d9bdd8e4d9d1be88722028310836f9a
michael <michael@12southmusic.com>
2014-10-23 15:56:09 -0500

    footer fix

--- templates/footer.php
+++ templates/footer.php
@@ -58,7 +58,7 @@
 			 endif;
 		  ?>
 			</div>
-			<div class="col-lg-3">
+			<div class="col-sm-6 col-lg-3">
 				<p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></p>
 			</div>
 	  </div>
